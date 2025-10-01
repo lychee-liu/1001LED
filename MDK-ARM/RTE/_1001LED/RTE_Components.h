fabd023de60b132906720be1de2247ee220084a3
lyc <lyc-lychee@sjtu.edu.cn>
2025-10-01 14:52:41 +0800

    10011452

---
+++ MDK-ARM/RTE/_1001LED/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '1001LED' 
+ * Target:  '1001LED' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
